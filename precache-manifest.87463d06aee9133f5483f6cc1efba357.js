72086d98d91c57a60cc28e9c2aa4fbe8d0352dd1
acdoyle630@gmail.com <acdoyle630@gmail.com>
2019-01-23 18:58:55 -0600

    Updates

--- precache-manifest.87463d06aee9133f5483f6cc1efba357.js
+++ precache-manifest.87463d06aee9133f5483f6cc1efba357.js
@@ -4,27 +4,27 @@ self.__precacheManifest = [
     "url": "/adam_doyle/static/media/logo.5ab808bf.svg"
   },
   {
-    "revision": "a3b2298ec71eaea021358254f44bb0b5",
-    "url": "/adam_doyle/static/media/Adam-Doyle-Resume.a3b2298e.pdf"
+    "revision": "96cef8c37b4bf55cf2e931cbf0abfa67",
+    "url": "/adam_doyle/static/media/Adam-Doyle-Resume.96cef8c3.pdf"
   },
   {
     "revision": "01996e614db555486dda",
     "url": "/adam_doyle/static/js/runtime~main.01996e61.js"
   },
   {
-    "revision": "9af53b1d130b83d3b65b",
-    "url": "/adam_doyle/static/js/main.9af53b1d.chunk.js"
+    "revision": "c0a07a2133ae97ffe189",
+    "url": "/adam_doyle/static/js/main.c0a07a21.chunk.js"
   },
   {
     "revision": "9c7dbe5c1bf19c949890",
     "url": "/adam_doyle/static/js/1.9c7dbe5c.chunk.js"
   },
   {
-    "revision": "9af53b1d130b83d3b65b",
+    "revision": "c0a07a2133ae97ffe189",
     "url": "/adam_doyle/static/css/main.fd3d0ce0.chunk.css"
   },
   {
-    "revision": "30b081944cec8e69a471fb6b66fe2a38",
+    "revision": "ab95a473efb5484ef0d862dbf40173f9",
     "url": "/adam_doyle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/adam_doyle/precache-manifest.86da89f03f324e6bef1cc850691f3053.js"
+  "/adam_doyle/precache-manifest.87463d06aee9133f5483f6cc1efba357.js"
 );
 
 workbox.clientsClaim();
