8cb0ed2387e3de23223ea90ea92fb3edf73f4336
acdoyle630@gmail.com <acdoyle630@gmail.com>
2021-07-18 15:56:27 -0700

    typo

--- src/modules/About/text/text.js
+++ src/modules/About/text/text.js
@@ -5,7 +5,7 @@ const text = {
   aboutTextP2:
     "Prior to my career in programming, I had a successful career in restaurant operations. My interest in creating system-level solutions and optimizations has proven to be one my biggest assets as an engineer.",
   aboutTextP3:
-    " In my career I have had the opportunity to work on a variety of prodcts with some awesome teams. I am always looking for new opportunities to explore new technologies, solve intersting problems and delivering value to users.",
+    " In my career I have had the opportunity to work on a variety of prodcts with some awesome teams. I am always looking for new opportunities to explore new technologies, solve intersting problems and deliver value to users.",
   skillsHeader: "Here's a few things I'm experienced with",
 };
 
